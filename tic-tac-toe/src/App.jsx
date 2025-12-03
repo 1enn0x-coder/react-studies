87256e56ba8c2dd05706bee79c019e630a4c6914
1enn0x-coder <selfimprovementisthekey2life@gmail.com>
2025-12-03 13:36:31 +0400

    change

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from "react";
-import Player from "./components/Players";
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
-export default function App() {
+function App() {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [activePlayer, setactivePlayer] = useState(1);
+  function handleCurrentPlayerChange() {
+    setCurrentPlayer((prevValue) => (prevValue === "X" ? "O" : "X"));
+    setactivePlayer((prevValue) => (prevValue === 1 ? 2 : 1));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player playername={"Player 1"} playersymbol={"X"}></Player>
-
-          <Player playername={"Player 2"} playersymbol={"O"}></Player>
+        <ol id="players" className="highlight-player">
+          <Player
+            currentPlayer={currentPlayer}
+            initialName="Player 1"
+            symbol="X"
+            active={activePlayer === 1}
+          />
+          <Player
+            currentPlayer={currentPlayer}
+            initialName="Player 2"
+            symbol="O"
+            active={activePlayer === 2}
+          />
         </ol>
+        <GameBoard
+          currentPlayer={currentPlayer}
+          onSquareSeelct={handleCurrentPlayerChange}
+        />
       </div>
     </main>
   );
 }
+
+export default App;
---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({ currentPlayer, initialName, symbol, active }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleEditClick() {
+    setIsEditing((prevValue) => !prevValue);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleNameChange}
+      />
+    );
+  }
+
+  return (
+    <li className={active ? "active" : ""}>
+      <span className={"player"}>
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+      </span>
+    </li>
+  );
+}
