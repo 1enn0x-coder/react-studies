a9523d1829b606ebc77dbfa5884ef18a115f6213
1enn0x-coder <selfimprovementisthekey2life@gmail.com>
2026-02-04 20:15:51 +0400

    change

--- quiz-game/src/components/QuizPage.jsx
+++ quiz-game/src/components/QuizPage.jsx
@@ -1,11 +1,67 @@
+import { useState } from "react";
+import { QUIZ_DATA } from "../quiz-questions.js";
+
 export default function QuizPage() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const currentQuestionData = QUIZ_DATA[currentQuestionIndex];
+  const [quizCompleted, setQuizCompleted] = useState(false);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  let answerChosen = false;
   return (
-    <>
-      <h1>What year did ww2 ended?</h1>
-      <p>1876</p>
-      <p>1980</p>
-      <p>1945</p>
-      <p>1958</p>
-    </>
-  );
+    
+
+  <>
+    {quizCompleted ? (
+      <h1>
+        Your quiz is completed! You got {correctAnswers} out of {QUIZ_DATA.length} correct.
+      </h1>
+    ) : (
+      <>
+        <h1>{currentQuestionData.question}</h1>
+
+        <div>
+          {currentQuestionData.answers.map((item, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                index === currentQuestionData.correct && !answerChosen
+                  ? (
+                      answerChosen = true,
+                      document.body.style.backgroundColor = "green",
+                      setCorrectAnswers(prev => prev + 1)
+                    )
+                  : (
+                      answerChosen = true,
+                      document.body.style.backgroundColor = "red"
+                    );
+              }}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+
+        <button
+          onClick={() => {
+            answerChosen && currentQuestionIndex < QUIZ_DATA.length - 1
+              ? (
+                  setCurrentQuestionIndex(prev => prev + 1),
+                  answerChosen = false,
+                  document.body.style.backgroundColor = "white"
+                )
+              : currentQuestionIndex >= QUIZ_DATA.length - 1
+              ? (
+                  document.body.style.backgroundColor = "blue",
+                  setQuizCompleted(true)
+                )
+              : alert("Please select an answer first!");
+          }}
+        >
+          Next Question
+        </button>
+      </>
+    )}
+  </>
+);
+        
 }
